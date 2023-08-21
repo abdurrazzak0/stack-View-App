ffc080714d19c9350298cb60d4523306dcb369dc
abdurrazzak0 <razzak6366@gmail.com>
2023-08-21 15:25:22 +0600

    Initial Commit

---
+++ stackView Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stackView Demo
+//
+//  Created by Abdur Razzak on 21/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
